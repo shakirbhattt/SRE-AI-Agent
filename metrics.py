5ee0c4eb37e1ada988f6041280be837716b81479
Shakir Bhat <50092163+shakirbhattt@users.noreply.github.com>
2026-03-10 11:40:55 +0530

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -2,7 +2,7 @@ def get_metrics():
     return {
         "service": "checkout",
         "latency_p95": 1.8,      # seconds
-        "error_rate": 12,        # %
-        "cpu": 40,               # %
+        "error_rate": 12,        # % percent
+        "cpu": 40,               # % percent
         "traffic_rpm": 1200
     }
